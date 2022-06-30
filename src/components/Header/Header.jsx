563e229bd6d0bf5cb619a83d052932df09047094
yorch82 <jcampo1982@outlook.com>
2022-06-30 18:04:50 +0200

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -16,53 +16,25 @@ const Header = () => {
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <div className="container-fluid">
         <span className="navbar-brand">Coding Social Network</span>
-        { user?
+        {user?
         <>
-          <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarColor01"
-          aria-controls="navbarColor01"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-          >
-          <span className="navbar-toggler-icon"></span>
-          </button>
+            <span><Link className="nav-link" to="/">
+                Home<span className="visually-hidden">(current)</span>
+              </Link></span>
             <span><Link className="nav-link" to="/" onClick={onLogout}>Logout</Link></span>
             <span><Link to="/profile" >{user.user.name}</Link></span>
         </>
         :
         <div className="collapse navbar-collapse" id="navbarColor01">
-          <ul className="navbar-nav me-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">
-                Home<span className="visually-hidden">(current)</span>
-              </Link>
-            </li>
+          <ul className="navbar-nav me-auto">            
             <li className="nav-item"></li>
             <li className="nav-item">
-              <Link className="nav-link" to="/login">
-                LOGIN
-              </Link>
+              <Link className="nav-link" to="/login">LOGIN</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/register">
-                REGISTER
-              </Link>
-            </li>
-           
-          </ul>
-          <form className="d-flex">
-            <input
-              className="form-control me-sm-2"
-              type="text"
-              placeholder="Search"
-            />
-            <button className="btn btn-secondary my-2 my-sm-0" type="submit">
-              Search
-            </button>
-          </form>
+              <Link className="nav-link" to="/register">REGISTER</Link>
+            </li>           
+          </ul>          
         </div> 
         }         
       </div>            
