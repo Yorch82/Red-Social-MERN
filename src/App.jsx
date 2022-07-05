83302073c50c67c957a57e371d66bb97915b5908
yorch82 <jcampo1982@outlook.com>
2022-07-05 11:31:25 +0200

    material UI login

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import "bootswatch/dist/spacelab/bootstrap.min.css";
 import "antd/dist/antd.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Profile from "./components/Profile/Profile";
 import PostDetail from "./components/Home/Posts/PostDetail/PostDetail";
 import Search from "../src/components/Search/Search";
-import WelcomeHome from "./components/WelcomeHome/WelcomeHome";
 import Admin from "./components/Admin/Admin";
 import "./styles.sass";
+import SignInSide from "./components/SignInSide/SignInSide";
 
 
 function App() {
@@ -19,10 +18,9 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" element={<WelcomeHome />} />
+          <Route path="/" element={<SignInSide />} />
           <Route path="/home" element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />          
           <Route path="/profile" element={<Profile />} />
           <Route path="/post/:_id" element={<PostDetail />} />
           <Route path="/search/:postTitle" element={<Search />} />
