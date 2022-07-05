180e8cc470005333ce940f10706f0553d06a9181
yorch82 <jcampo1982@outlook.com>
2022-07-05 16:21:26 +0200

    register material ui

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,57 +1,73 @@
+import * as React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
-import { getInfo, reset } from "../../features/posts/postsSlice"
-import { Card } from "antd";
+import { getInfo, reset } from "../../features/posts/postsSlice";
+import Card from "@mui/material/Card";
+// import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+// import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
 const API_URL = "http://localhost:8080";
-const { Meta } = Card;
+
 
 const Profile = () => {
   const { user } = useSelector((state) => state.auth);
 
   const { userPosts } = useSelector((state) => state.posts);
-  
+
   const postIds = userPosts.postIds;
 
   const dispatch = useDispatch();
 
   const getUserPosts = async () => {
     await dispatch(getInfo());
-    dispatch(reset())
+    dispatch(reset());
   };
 
   useEffect(() => {
-    getUserPosts()
+    getUserPosts();
     // eslint-disable-next-line
   }, []);
 
   const userPost = postIds?.map((userPost) => {
     return (
-        <div key={userPost._id}>
-            <Link to={"/post/" + userPost._id}>
-                <p>{userPost.title}</p>
-            </Link>
-        </div>
-    )
-  })
+      <div key={userPost._id}>
+        <Link to={"/post/" + userPost._id}>
+          <p>{userPost.title}</p>
+        </Link>
+      </div>
+    );
+  });
 
   return (
     <>
       <div>
-        <h1>Profile</h1>
-        <Card
-          hoverable
-          style={{ width: 240 }}
-          cover={<img alt="your ugly face" src={API_URL + user.user.avatar} />}>
-        <Meta title={user.user.name} description={user.user.mail} />
-      </Card>
+        <h1>Profile</h1>        
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            component="img"
+            alt="your ugly face"
+            height="250"
+            image={API_URL + user.user.avatar}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+            {user.user.name}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+            {user.user.mail}
+            </Typography>
+          </CardContent>          
+        </Card>
       </div>
       <div>
         <h3>Posts</h3>
         <div>{userPost}</div>
       </div>
     </>
-    
   );
 };
 
